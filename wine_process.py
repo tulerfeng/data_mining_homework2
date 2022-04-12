308775adcb5302e54ae6781da3aca62da4cf1cd3
tulerfeng <50584334+tulerfeng@users.noreply.github.com>
2022-04-12 12:53:16 +0800

    Update wine_process.py

--- wine_process.py
+++ wine_process.py
@@ -9,6 +9,7 @@ import pytorch_geometric
 import dgl
 import networkx
 from scipy import fft
+import torch.nn as nn
 
 
 plt.rcParams["font.sans-serif"]=["SimHei"]
@@ -51,6 +52,47 @@ def scanD( D, Ck, minSupport ):
             retList.insert( 0, key )
             supportData[ key ] = support
     return retList, supportData
+import torch.nn as nn
+
+class PairNorm(nn.Module):
+    def __init__(self, mode='PN-SCS', scale=1.0):
+
+        assert mode in ['None', 'PN',  'PN-SI', 'PN-SCS']
+        super(PairNorm, self).__init__()
+        self.mode = mode
+        self.scale = scale
+
+        
+    def forward(self, x):
+        if self.mode == 'None':
+            return x
+        
+        col_mean = x.mean(dim=0)      
+        if self.mode == 'PN':
+            x = x - col_mean
+            rownorm_mean = (1e-6 + x.pow(2).sum(dim=1).mean()).sqrt() 
+            x = self.scale * x / rownorm_mean
+
+        if self.mode == 'PN-SI':
+            x = x - col_mean
+            rownorm_individual = (1e-6 + x.pow(2).sum(dim=1, keepdim=True)).sqrt()
+            x = self.scale * x / rownorm_individual
+
+        if self.mode == 'PN-SCS':
+            rownorm_individual = (1e-6 + x.pow(2).sum(dim=1, keepdim=True)).sqrt()
+            x = self.scale * x / rownorm_individual - col_mean
+
+        return x
+
+
+class DynamicPairNorm(nn.Module):
+
+    def __init__(self):
+        super(DynamicPairNorm, self).__init__()
+    
+    def __TransFeauture(self):
+
+
 
 def aprioriGen( Lk, k ):
     retList = []
